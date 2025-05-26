652e3ab66f6482c8d57f86c7cbd6676b56db9330
Kirpicheva Daria <118064855+Kirpicheva04@users.noreply.github.com>
2025-05-26 14:39:51 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,19 +1,11 @@
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
-# Импортируем действия из других файлов
 from actions.actions_time import ActionGetTime
 from actions.actions_weather import ActionGetWeather
 from actions.actions_calculate import ActionCalculate
 from actions.actions_web_search import ActionWebSearch
 from actions.actions_analyze_sentiment import ActionAnalyzeSentiment
 from actions.actions_joke import ActionRandomSong
-#from actions.actions_remember_name import ActionRememberName
-#from actions.actions_get_name import ActionGetName
 from actions.actions_memory import ActionSaveUserMemory, ActionLoadUserMemory
-from actions.actions_name import ActionRememberName, ActionGreetWithName
+from actions.name_actions import ActionRememberName, ActionGetName
 
 __all__ = [
     "ActionGetTime",
@@ -23,7 +15,7 @@ __all__ = [
     "ActionAnalyzeSentiment",
     "ActionRandomSong",
     "ActionRememberName",
-    "ActionGreetWithName",
+    "ActionGetName",
     "ActionSaveUserMemory",
     "ActionLoadUserMemory",
 ]
