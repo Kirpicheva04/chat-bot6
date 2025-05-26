00a465f58375a01308a7597cf2fe56a66c80352c
Kirpicheva Daria <118064855+Kirpicheva04@users.noreply.github.com>
2025-05-26 14:40:38 +0300

    Update actions_name.py

--- actions_name.py
+++ actions_name.py
@@ -1,27 +1,36 @@
-# actions.py (или actions_memory.py)
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+import spacy
+from typing import Any, Text, Dict, List
 
-class ActionRememberName(Action):
-    def name(self):
+nlp = spacy.load("ru_core_news_sm")
+
+class ActionRememberName(Action): # Это не нужно, т.к. имя не нужно извлекать
+    def name(self) -> Text:
         return "action_remember_name"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
-        name = tracker.get_slot("name")
-        if name:
-            return [SlotSet("name", name)] # Только сохраняем имя в слоте
-        else:
-            return []
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        lang = domain.get("config", {}).get("language", "ru")
+        text = tracker.latest_message.get("text")
+        # doc = nlp(text) # не нужно, если используем извлечение из NLU
+        # name = next((ent.text for ent in doc.ents if ent.label_ == "PER"), None) # не нужно, если используем извлечение из NLU
+        name = tracker.get_slot("name") # Получаем имя из слота, если оно было извлечено из NLU
+        if not name:
+            dispatcher.utter_message(response="utter_ask_name")
+            return [] # если имени нет, просим его ввести
+        message = f"Привет, {name}!" if lang == "ru" else f"Nice to meet you, {name}! Меня зовут Ботик."
+        dispatcher.utter_message(text=message)
+        return [SlotSet("name", name), SlotSet("last_bot_message", message)]
 
-class ActionGreetWithName(Action): # Новое действие
-    def name(self):
-        return "action_greet_with_name"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
+class ActionGetName(Action):
+    def name(self) -> Text:
+        return "action_get_name"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        lang = domain.get("config", {}).get("language", "ru")
         name = tracker.get_slot("name")
-        if name:
-            dispatcher.utter_message(text=f"Рад тебя видеть снова, {name}!")
-        else:
-            dispatcher.utter_message(text="Рад познакомиться!")
-        return []
+        message = f"Тебя зовут {name}, верно?" if name and lang == "ru" else f"Your name is {name}, right?" if name else "Я пока не знаю, как тебя зовут." if lang == "ru" else "I don't know your name yet.";
+        dispatcher.utter_message(text=message)
+        return [SlotSet("last_bot_message", message)]
